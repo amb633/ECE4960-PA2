5f303698de30757576483685241874282042bcf9
hm535 <hm535@users.noreply.github.com>
2018-03-07 20:35:22 -0500

    function templates

---
+++ full_func.hpp
@@ -0,0 +1,40 @@
+#ifndef full_mat_func_hpp
+#define full_mat_func_hpp
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <numeric>
+#include <stdio.h>
+
+using namespace std;
+
+namespace full{
+
+	/*typedef struct Full_Matrix{
+		vector<double> value;
+    	vector<size_t> row_p;
+    	vector<int> col_id;
+	} full_mat;
+
+	typedef struct Full_Diagonal_Matrix{
+		vector<double> value;
+		vector<size_t> rank;		
+	} full_diag;*/
+	double retrieveElement( vector<vector<double>>* input, int row_id, int col_id);
+	double productAx( vector< vector<double>>* input, vector<double>* x, vector<double>* b);
+	void rowScale(vector< vector<double>>* input, int i, int j, double a );
+	void rowPermute(vector< vector<double>>* input, int i, int j);
+
+	void print_full_mat( vector< vector<double>>* input );
+	//bool check_sum( vector< vector<double>>* mat, vector<double>* vec );
+	void reorderMat( vector< vector<double>>* input, vector< vector<double>>* reorder_A, vector< vector<double>>* reorder_B, int R, int C);
+	void columnPermute(vector< vector<double>>* A, int col1, int col2);
+
+	int changeElement( vector< vector<double>>* A , int rowInd , int colInd , double newValue );
+	int copyMatrix( vector< vector<double>>* C , vector< vector<double>>* A );
+	int decomposeMatrix( vector<double>* D , vector< vector<double>>* LU , vector< vector<double>>* A );
+	int jacobiSolver( vector<double>* X , vector<double>* D , vector< vector<double>>* LU , vector<double>* B );
+	int calculateNorm( double& norm , vector<double>* v , vector<double>* Ax );
+
+}
